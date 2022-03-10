a32d286e260d88c904785de743ae78986960d541
HailOlucifer <90060588+RawatShorya@users.noreply.github.com>
2022-03-10 22:23:28 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <div class="topNavbar">
+      <p class="heading">Academia - Academic Web Services</p>
+      <h4 class="id">sr8499</h4>
+    </div>
+
+    <div class="topnav">
+      <a class="active" href="#Welcome">
+        Welcome
+      </a>
+      <a href="#Circular">Circular</a>
+      <a href="#Student_Profile">Student Profile</a>
+      <a href="#Academic_Report">Academic Report</a>
+      <a href="#Student">Student</a>
+      <a href="#Unified_TimeTable">Unified TimeTable</a>
+      <a href="#Course_Feedback">Course Feedback</a>
+    </div>
+    <div>
+      <img src="front.png" alt=""></img>
+    </div>
+  </div>,
+  document.getElementById("root")
+);
